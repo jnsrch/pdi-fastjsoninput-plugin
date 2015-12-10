3cfc2b48080a320a6d64890af9445b58faededf7
tgf <tiago.n.ferreira@gmail.com>
2015-12-10 15:16:16 +0000

    Update pentaho6,vfs2,deprecated; fixes #3

--- src/main/java/com/trail2peak/pdi/fastjsoninput/FastJsonInput.java
+++ src/main/java/com/trail2peak/pdi/fastjsoninput/FastJsonInput.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedHashMap;
 
-import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs2.FileObject;
 import org.json.simple.JSONObject;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.ResultFile;
